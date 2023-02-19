33e2b4069357e72dbc37de7c27fd00708703d673
Tran Quy Manh - 21020352 <21020352@vnu.edu.vn>
2023-02-19 15:28:41 +0700

    variables in js

--- video2/video2.js
+++ video2/video2.js
@@ -1,1 +1,14 @@
-console.log('hello everybody my name is andrew!')
+var x = 2;
+let y = 4;
+const z = x % y;
+{
+    x = 4;
+}
+console.log('hello everybody my name is andrew!');
+console.log('is x equal y: ', z);
+
+const music = ["your lie in April", "living in the missing", "admiting the wrong"];
+music[2] = "shape of you";
+
+document.getElementById("demo").innerHTML = "show x + y = " + x + "show the music: " + music.pop() + " " + music.pop() ;
+document.write(music.pop());
