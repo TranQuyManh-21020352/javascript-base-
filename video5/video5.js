1b60e0352ad3384e0fa83a22f7a77cd768418964
Tran Quy Manh - 21020352 <21020352@vnu.edu.vn>
2023-02-23 21:08:33 +0700

    Array and default  method

--- video5/video5.js
+++ video5/video5.js
@@ -1,51 +1,90 @@
-
-
-const a = 4;
-let i = 1;
-while(i <= 10){
-console.log(
-    `luy thua bac i: ${a ** i}`
-);
-i++;
-}
-
-let array = ['Arsenal', 'ManchesterCity', 'ManchesterUnited', 'Liverpool'];
-console.log(array[0].concat(' ' + array[2]));
-// let i = {};
-// for(let i = 0; i < array.length; i ++){
-//     if(i == 0)
-//     console.log(array[i]);
-// }
-// console.log(i);
-
-function myFunction(){
+function myFunction() {
     let output = '';
     let age = Number(document.getElementById('age').value);
-    if(isNaN(age)){
+    if (isNaN(age)) {
         output = 'please input your age by number';
-    }else{
+    } else {
         output = (age < 18) ? 'you are not enough old' : 'you are enough old';
     }
     document.getElementById('demo').innerHTML = output + ' to vote!';
 }
 
-let test = 39/'ab';
-console.log(isNaN(test));
+class User {
+    constructor(firsName, lastName, avatar) {
+        this.firsName = firsName;
+        this.lastName = lastName;
+        this.avatar = avatar;
 
-let arr = [24, 0, 25];
-console.log(arr);
-console.log(arr.join(', '));// join()
-console.log(arr.pop());//remove the first element of array
-console.log(arr.pop());
-console.log(arr.pop());
-console.log(arr.push(28,29,30));// add one element into array
-console.log(arr.unshift(45, 46, 48));// insert onto the first element
-arr.splice(1, 2);// remove n elements from the start 
-console.log(arr);
-console.log(arr.slice(0));
+        this.getName = function () {
+            return `${this.firsName} ${this.lastName}`;
+        };
 
+        this.getAge = function () {
+            return this.avatar;
+        }
+    }
+}
 
+let date = new Date();
+console.log(`Hom nay la ngay ${date.getDate()} thang ${date.getMonth() + 1} nam ${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`);
+let MU = new User('Thanh', 'Pham Duc', 'Avatar');
+let Barca = new User('Manh', 'Tran Quy', 19);
+console.log(MU.getName());
+console.log(Barca.getAge());
 
+let day = 3;
+switch (day) {
+    case 1:
+        console.log('Hom nay la thu hai');
+        break;
+    case 2:
+        console.log('Hom nay la thu ba');
+        break;
+    case 3:
+        console.log('Hom nay la thu tu');
+        break;
+    default:
+        console.log('Hom nay la thu tam');
+}
 
+let course = [{
+    name: 'os',
+    id: '1',
+    coin: 0
+},
+{
+    name: 'dbms',
+    id: '2',
+    coin: 0
+},
+{
+    name: 'se',
+    id: '3',
+    coin: 5
+}]
 
+// for(let key in course){
+//     console.log(course[key]);
+// }
 
+let isFree = course.every(function(course, index){
+    return course.coin === 0;
+});
+
+let newCourses = course;
+for(let value of course){
+console.log(`<h2>${value.name}</h2>`);
+}
+
+function sumCoin(accumuator, currentValue, currentIndex, currentArray ){
+    return accumuator + currentValue.coin;
+}
+
+let totalCoin = course.reduce(sumCoin, 0);
+console.log((totalCoin > 0) ? 'phai tra phi' : 'mien phi');
+
+let depthArray = [1, 2, [3, 4], 5, 6 , [7, 8, 9]];
+let outArray  = depthArray.reduce(function(pre, current){
+    return pre.concat(current);
+}, []);
+console.log(outArray.length);
